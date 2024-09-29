6902a32b6aafce62dd4c250be2aa265eb8579eae
mukherjee-pinakpani <mukherjee.pinakpani14@gmail.com>
2024-09-29 23:31:27 +0900

    Added listview

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import ProjectHeader from "./ProjectHeader";
 import BoardView from "./BoardView";
+import ListView from "./ListView";
 
 type Props = {
   params: { id: string };
@@ -18,6 +19,9 @@ const Project = ({ params }: Props) => {
       {<ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />}
       {activeTab === "Board" && (
         <BoardView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+      {activeTab === "List" && (
+        <ListView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
       )}
     </div>
   );
