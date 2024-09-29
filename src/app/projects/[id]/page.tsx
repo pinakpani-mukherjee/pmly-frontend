87440afefc6b655f6c34379f91655e0094d62119
mukherjee-pinakpani <mukherjee.pinakpani14@gmail.com>
2024-09-30 00:39:36 +0900

    added the gantt timeline chart

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 import { useState } from "react";
 import ProjectHeader from "./ProjectHeader";
-import BoardView from "./BoardView";
-import ListView from "./ListView";
+import BoardView from "../BoardView";
+import ListView from "../ListView";
+import TimelineView from "../TimelineView";
 
 type Props = {
   params: { id: string };
@@ -22,6 +23,9 @@ const Project = ({ params }: Props) => {
       )}
       {activeTab === "List" && (
         <ListView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+      {activeTab === "Timeline" && (
+        <TimelineView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
       )}
     </div>
   );
