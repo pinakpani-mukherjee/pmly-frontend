a772d6be41b0342d0b24791ab3fcf466c57bb9b6
mukherjee-pinakpani <mukherjee.pinakpani14@gmail.com>
2024-10-04 01:40:00 +0900

    added the teams page

--- src/app/state/api.ts
+++ src/app/state/api.ts
@@ -64,11 +64,17 @@ export interface SearchResults {
     projects?: Project[]
     users?: User[]
 }
+export interface Team {
+    teamId: number;
+    teamName: string;
+    productOwnerUserId?: number;
+    projectManagerUserId?: number;
+  }
 
 export const api = createApi({
     baseQuery: fetchBaseQuery({baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL}),
     reducerPath: "api",
-    tagTypes: ["Projects","Tasks","Users"],
+    tagTypes: ["Projects","Tasks","Users","Teams"],
     endpoints: (build) => ({
         getProjects: build.query<Project[],void>({
             query: () => "projects",
@@ -109,14 +115,14 @@ export const api = createApi({
             query: () => "users",
             providesTags: ["Users"],
           }),
-          /* getTeams: build.query<Team[], void>({
+          getTeams: build.query<Team[], void>({
             query: () => "teams",
             providesTags: ["Teams"],
-          }), */
+          }),
         search: build.query<SearchResults,string>({
             query:(query) => `search?query=${query}`
         })
     }),
 });
 
-export const {useGetProjectsQuery,useCreateProjectMutation, useGetTasksQuery, useCreateTaskMutation,useUpdateTaskStatusMutation,useSearchQuery, useGetUsersQuery} = api;
+export const {useGetProjectsQuery,useCreateProjectMutation, useGetTasksQuery, useCreateTaskMutation,useUpdateTaskStatusMutation,useSearchQuery, useGetUsersQuery,useGetTeamsQuery} = api;
